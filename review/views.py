d727cf27563748ed9642b35106459869af7f9fdc
Amrit Khera <amritkhera98@google.com>
2020-11-03 21:31:13 +0530

    add home and search views

--- review/views.py
+++ review/views.py
@@ -1,8 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+from datetime import datetime
+
 def home(request):
-    return render(request, "home.html")
+    movies = Movie.objects.all()
+    upcoming_movies = []
+    current_movies = []
+    for movie in movies:
+        if movie.release_date <= datetime.date(datetime.now()):
+            current_movies.append(movie)
+        else:
+            upcoming_movies.append(movie)
+    
+    context = {}
+    context["upcoming_movies"] = upcoming_movies
+    context["current_movies"] = current_movies
+
+    return render(request, "home.html", context)
 
 def feed(request):
     return HttpResponse("<h1>Feed</h1>")
+
+def search(request):
+    if request.method == "GET":
+        return render(request, "search.html")
+
+    elif request.method == "POST":
+        print(request.POST)
+
+        movies_names = movies_genres = movies_year = movies_language = movies_rating = Movie.objects.all()
+
+        if(request.POST["name"]!='any'):
+            movies_names = Movie.objects.filter(name__icontains = request.POST["name"])
+
+        if(request.POST["genre"]!='any'):
+            movies_genres = Movie.objects.filter(genre = request.POST["genre"])
+
+        if(request.POST["date_start"]!='1900-01-01' and request.POST["date_end"]!='2200-01-01'):
+            movies_year = Movie.objects.filter(release_date__range = [request.POST["date_start"], request.POST["date_end"]])
+
+        if(request.POST["language"]!='any'):
+            movies_language = Movie.objects.filter(language = request.POST["language"])
+
+        if(request.POST["rating"]!='0'):
+            movies_rating = Movie.objects.filter(rating = request.POST["rating"])
+
+        searched_movies = movies_names & movies_genres & movies_year & movies_language & movies_rating
+
+        for movie in searched_movies:
+            print(movie.name)
+        
+        context = {}
+        context["searched_movies"] = searched_movies
+
+        return render(request, "search_results.html", context)
