803c9a7a881a7d1fa826395663aaaf36b1e58e4a
Amrit Khera <amritkhera98@google.com>
2020-11-03 21:29:38 +0530

    add admin model

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Movie, Review
 
 # Register your models here.
+
+# admin.site.register(User)
+admin.site.register(Movie)
+admin.site.register(Review)
