ef43e2cc42878ed8091abe0527166d6ae9fd975c
Amrit Khera <amritkhera98@google.com>
2020-11-03 23:19:42 +0530

    add movie page url

--- review/urls.py
+++ review/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('movie/<int:id>', views.movie, name="movie"),
     path('feed/', views.feed, name='feed'),
     path('search/', views.search, name='search'),
 ]
